d96208114ecb01d9bd44a94abc0f60634341aae1
Merritt Baggett <mertbagt@gmail.com>
2020-12-31 10:15:34 -0500

    tweaked logger, fixed .split in validate-bearer-token

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -4,7 +4,7 @@ const logger = require('./logger')
 function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization')
       
-  if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
+  if (!authToken || authToken.split(' ')[0] !== API_TOKEN) {
     logger.error(`Unauthorized request to path: ${req.path}`);
     return res
       .status(401)
